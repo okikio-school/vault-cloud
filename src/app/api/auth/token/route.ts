e37da2c00dc01fd0815c02cfbc5d22074d378d79
Okiki Ojo <okikioluwa.ojo@ontariotechu.net>
2024-11-01 20:31:34 +0000

    chore: ...

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -5,11 +5,13 @@ import type { AuthOptions } from "next-auth";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../[...nextauth]/_options";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, authOptions as unknown as AuthOptions);
+export async function GET(req: Request) {
+  const session = await getServerSession(authOptions as unknown as AuthOptions);
   if (session) {
-    res.status(200).json({ token: session.accessToken });
+    // res.status(200).json({ token: session.accessToken });
+    return Response.json({ token: session.accessToken }, { status: 200 });
   } else {
-    res.status(401).json({ message: "Unauthorized" });
+    // res.status(401).json({ message: "Unauthorized" });
+    return Response.json({ message: "Unauthorized" }, { status: 401 });
   }
 }
